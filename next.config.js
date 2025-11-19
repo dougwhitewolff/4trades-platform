2137f79402a86bf9b7d15f2397195b85c35ccfb9
Azmain Morshed <azmain.morshed3@gmail.com>
2025-11-19 14:29:29 +0600

    image optimization and fast refresh

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [],
+  },
+  // Ensure Fast Refresh is enabled (default is true)
+  experimental: {
+    forceSwcTransforms: true,
   },
 }
 
